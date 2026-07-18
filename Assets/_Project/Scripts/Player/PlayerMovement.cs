7ad6bebec405ea47693104ea3cc0f8fe4349df39
Diego <d.mayorga01@ufromail.cl>
2026-07-18 14:54:39 -0400

    feat: add PlayerMovement with run, jump, and ground detection

---
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+namespace Project.Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(PlayerInputHandler))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [Header("Movement")]
+        [SerializeField] private float _speed = 150f;
+        [SerializeField] private float _jumpVelocity = 300f;
+
+        [Header("Ground Check")]
+        [SerializeField] private Transform _groundCheck;
+        [SerializeField] private float _groundCheckRadius = 0.1f;
+        [SerializeField] private LayerMask _groundLayer;
+
+        private Rigidbody2D _rb;
+        private PlayerInputHandler _input;
+
+        public bool IsGrounded { get; private set; }
+        public bool IsMoving { get; private set; }
+        public bool FacingRight { get; private set; } = true;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _input = GetComponent<PlayerInputHandler>();
+        }
+
+        private void FixedUpdate()
+        {
+            CheckGrounded();
+            HandleJump();
+            HandleHorizontalMovement();
+            _input.ConsumeJumpFlags();
+        }
+
+        private void CheckGrounded()
+        {
+            IsGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundLayer);
+        }
+
+        private void HandleJump()
+        {
+            if (_input.JumpPressed && IsGrounded)
+            {
+                _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _jumpVelocity);
+            }
+        }
+
+        private void HandleHorizontalMovement()
+        {
+            float direction = _input.MoveInput.x;
+            IsMoving = Mathf.Abs(direction) > 0.01f;
+
+            if (IsMoving)
+            {
+                FacingRight = direction > 0;
+                _rb.linearVelocity = new Vector2(direction * _speed, _rb.linearVelocity.y);
+            }
+            else
+            {
+                float newX = Mathf.MoveTowards(_rb.linearVelocity.x, 0, _speed * Time.fixedDeltaTime);
+                _rb.linearVelocity = new Vector2(newX, _rb.linearVelocity.y);
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (_groundCheck == null) return;
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(_groundCheck.position, _groundCheckRadius);
+        }
+    }
+}
